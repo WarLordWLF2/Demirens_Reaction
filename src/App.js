9e4c4be96cdfb0dcb286f2054634898803425be8
WarLordWLF2 <158793263+WarLordWLF2@users.noreply.github.com>
2025-08-22 11:00:23 +0800

    Almost done setting up...

--- src/App.js
+++ src/App.js
@@ -21,12 +21,20 @@ import AdminNewBook from './pages/admin/WalkIn_Folder/AddWalkIn';
 import Landingpage from './pages/Landingpage';
 import Billings from './pages/admin/Billings';
 import Invoice from './pages/admin/Invoice';
-import OnlineReqList from './pages/admin/OnlineReqList';
+
+// Online
+import OnlineReqList from './pages/admin/Online_Folder/OnlineReqList';
+import { ApprovalProvider } from './pages/admin/Online_Folder/ApprovalContext';
+import ApproveRooms from './pages/admin/Online_Folder/ApproveRooms';
+import ApprovalReceipt from './pages/admin/Online_Folder/ApprovalReceipt';
+
+// Walk In
 import AddWalkIn from './pages/admin/WalkIn_Folder/AddWalkIn';
 import ChooseRooms from './pages/admin/WalkIn_Folder/ChooseRooms';
 import PaymentMethod from './pages/admin/WalkIn_Folder/PaymentMethod';
 import Confirmation from './pages/admin/WalkIn_Folder/Confirmation';
 import { WalkInProvider } from './pages/admin/WalkIn_Folder/WalkInContext';
+
 
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -70,55 +78,62 @@ function App() {
 
       <Router>
         <WalkInProvider>
-          <div style={{ flex: 1, padding: '20px' }}>
-            <Routes>
-              {/* Admin Pages */}
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/" element={<Landingpage />} />
-              <Route path="/admin/dashboard" element={<AdminDashboard />} />
-              <Route path="/admin/profile" element={<AdminProfile />} />
-              <Route path="/admin/roomslist" element={<AdminRoomsList />} />
-              <Route path="/admin/bookinglist" element={<AdminBookingList />} />
-              <Route path="/admin/newbook" element={<AdminNewBook />} />
-              <Route path="/admin/calendar" element={<AdminCalendar />} />
-              <Route path="/admin/guestprofile" element={<AdminGuestProfile />} />
-              <Route path="/admin/payments" element={<AdminPayments />} />
-              <Route path="/admin/requestedamenities" element={<AdminRequestedAmenities />} />
-              <Route path="/admin/reviews" element={<AdminReviews />} />
-              <Route path="/admin/transactionhistory" element={<AdminTransactionHis />} />
-              <Route path="/admin/visitorslog" element={<AdminVisitorsLog />} />
-              <Route path="/admin/amenitymaster" element={<AdminAmenityMaster />} />
-              <Route path="/admin/chargescategory" element={<AdminChargesCategory />} />
-              <Route path="/admin/chargemaster" element={<AdminChargeMaster />} />
-              <Route path="/admin/discountmaster" element={<AdminDiscountMaster />} />
-              <Route path="/admin/roomtypemaster" element={<AdminRoomtype />} />
-              <Route path="/admin/billings" element={<Billings />} />
-              <Route path="/admin/invoice" element={<Invoice />} />
-              <Route path="/admin/online" element={<OnlineReqList />} />
+          <ApprovalProvider>
+            <div style={{ flex: 1, padding: '20px' }}>
+              <Routes>
+                {/* Admin Pages */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<Landingpage />} />
+                <Route path="/admin/dashboard" element={<AdminDashboard />} />
+                <Route path="/admin/profile" element={<AdminProfile />} />
+                <Route path="/admin/roomslist" element={<AdminRoomsList />} />
+                <Route path="/admin/bookinglist" element={<AdminBookingList />} />
+                <Route path="/admin/newbook" element={<AdminNewBook />} />
+                <Route path="/admin/calendar" element={<AdminCalendar />} />
+                <Route path="/admin/guestprofile" element={<AdminGuestProfile />} />
+                <Route path="/admin/payments" element={<AdminPayments />} />
+                <Route path="/admin/requestedamenities" element={<AdminRequestedAmenities />} />
+                <Route path="/admin/reviews" element={<AdminReviews />} />
+                <Route path="/admin/transactionhistory" element={<AdminTransactionHis />} />
+                <Route path="/admin/visitorslog" element={<AdminVisitorsLog />} />
+                <Route path="/admin/amenitymaster" element={<AdminAmenityMaster />} />
+                <Route path="/admin/chargescategory" element={<AdminChargesCategory />} />
+                <Route path="/admin/chargemaster" element={<AdminChargeMaster />} />
+                <Route path="/admin/discountmaster" element={<AdminDiscountMaster />} />
+                <Route path="/admin/roomtypemaster" element={<AdminRoomtype />} />
+                <Route path="/admin/billings" element={<Billings />} />
+                <Route path="/admin/invoice" element={<Invoice />} />
 
-              {/* WalkIn Pages */}
-              <Route path="/admin/add-walk-in" element={<AddWalkIn />} />
-              <Route path="/admin/choose-rooms" element={<ChooseRooms />} />
-              <Route path="/admin/payment-method" element={<PaymentMethod />} />
-              <Route path="/admin/confirmation" element={<Confirmation />} />
+                {/* Online Pages */}
+                <Route path="/admin/online" element={<OnlineReqList />} />
+                <Route path="/admin/approve/:bookingId" element={<ApproveRooms />} />
+                <Route path="/admin/receipt/:bookingId" element={<ApprovalReceipt />} />
 
-              {/* Frontdesk Pages */}
-              <Route path="/frontdesk/login" element={<FrontdeskLogin />} />
-              <Route path="/frontdesk/dashboard" element={<FrontdeskDashboard />} />
-              <Route path="/frontdesk/profile" element={<FrontdeskProfile />} />
-              <Route path="/frontdesk/roomslist" element={<FrontdeskReservation />} />
-              <Route path="/frontdesk/walkin" element={<FrontdeskWalkin />} />
+                {/* WalkIn Pages */}
+                <Route path="/admin/add-walk-in" element={<AddWalkIn />} />
+                <Route path="/admin/choose-rooms" element={<ChooseRooms />} />
+                <Route path="/admin/payment-method" element={<PaymentMethod />} />
+                <Route path="/admin/confirmation" element={<Confirmation />} />
 
-              {/* Customer Pages */}
-              <Route path="/customer/about" element={<CustomerAbout />} />
-              <Route path="/customer/bookings" element={<CustomerBooking />} />
-              <Route path="/customer/rooms" element={<CustomerRooms />} />
-              <Route path="/customer/gallery" element={<CustomerGallery />} />
-              <Route path="/customer/restaurant" element={<CustomerRestaurant />} />
-              <Route path="/customer" element={<CustomerMain />} />
-            </Routes>
-          </div>
+
+                {/* Frontdesk Pages */}
+                <Route path="/frontdesk/login" element={<FrontdeskLogin />} />
+                <Route path="/frontdesk/dashboard" element={<FrontdeskDashboard />} />
+                <Route path="/frontdesk/profile" element={<FrontdeskProfile />} />
+                <Route path="/frontdesk/roomslist" element={<FrontdeskReservation />} />
+                <Route path="/frontdesk/walkin" element={<FrontdeskWalkin />} />
+
+                {/* Customer Pages */}
+                <Route path="/customer/about" element={<CustomerAbout />} />
+                <Route path="/customer/bookings" element={<CustomerBooking />} />
+                <Route path="/customer/rooms" element={<CustomerRooms />} />
+                <Route path="/customer/gallery" element={<CustomerGallery />} />
+                <Route path="/customer/restaurant" element={<CustomerRestaurant />} />
+                <Route path="/customer" element={<CustomerMain />} />
+              </Routes>
+            </div>
+          </ApprovalProvider>
         </WalkInProvider>
       </Router>
     </>
